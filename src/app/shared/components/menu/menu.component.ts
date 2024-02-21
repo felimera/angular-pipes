088acd0aa1d1efb0e725bc53c2f794666d02f399
andres.mera <feli041089@gmail.com>
2024-02-21 10:50:10 -0500

    dev : PrimeNg - Menubar

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -13,13 +13,33 @@ export class MenuComponent implements OnInit {
   ngOnInit() {
     this.menuItems = [
       {
-        label: 'New',
-        icon: 'pi pi-fw pi-plus',
+        label: 'Pipe de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left'
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar'
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe'
+          },
+        ]
       },
       {
-        label: 'Delete',
-        icon: 'pi pi-fw pi-trash'
+        label: 'Pipe personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-cog',
+          }
+        ]
       }
-    ];
+    ]
   }
 }
