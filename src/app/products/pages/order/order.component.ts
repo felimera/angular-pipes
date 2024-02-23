b1e9c5782f370332251bb654d66c56ebb2f2f95c
andres.mera <feli041089@gmail.com>
2024-02-23 08:29:20 -0500

    dev : Argumentos dinámicos a pipes personalizados.

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class OrderComponent {
 
+  public isUpperCase: boolean = false;
+
+  public toggleUpperCase(): void {
+    this.isUpperCase = !this.isUpperCase;
+  }
 }
