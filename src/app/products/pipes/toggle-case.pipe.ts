17d4ab22309e0895e37503244f82587e54bf8f34
andres.mera <feli041089@gmail.com>
2024-02-23 08:23:22 -0500

    dev : Valores y argumentos personalizados al Pipe.

--- src/app/products/pipes/toggle-case.pipe.ts
+++ src/app/products/pipes/toggle-case.pipe.ts
@@ -5,7 +5,9 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class ToggleCasePipe implements PipeTransform {
 
-  transform(value: string): string {
-    return value.toLocaleUpperCase();
+  transform(value: string, toUpper: boolean = false): string {
+    return (toUpper)
+      ? value.toUpperCase()
+      : value.toLowerCase();
   }
 }
